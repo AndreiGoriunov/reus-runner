d81a77efbdd5113cacdd3af2b38577ca40296874
AndreiGoriunov <52248218+AndreiGoriunov@users.noreply.github.com>
2024-04-04 22:07:34 -0400

    Template files

---
+++ build.py
@@ -0,0 +1,50 @@
+import os
+import shutil
+import time
+
+from PyInstaller.__main__ import run as pyinstaller_run
+
+# Feel free to change the following values
+exe_name = "reus-runner.exe"
+main_file = "main.py"
+
+
+def main():
+
+    # Directories to remove
+    directories = ["dist", "build", "release"]
+
+    # Remove dist, build, and release directories if they exist
+    for dir_ in directories:
+        if os.path.exists(dir_):
+            shutil.rmtree(dir_)
+
+    # Remove all __pycache__ directories
+    for root, dirs, files in os.walk("."):
+        for dir_ in dirs:
+            if dir_ == "__pycache__":
+                shutil.rmtree(os.path.join(root, dir_))
+
+    # Run pyinstaller programmatically
+    pyinstaller_run(["--onefile", main_file])
+
+    # Create 'release' directory if it doesn't exist
+    os.makedirs("release", exist_ok=True)
+
+    print("Copying files into 'release' directory...")
+
+    # Copy the config file to the release folder
+    shutil.copy("config.properties", os.path.join("release", "config.properties"))
+
+    # Wait for a few seconds (adjust the time as needed)
+    time.sleep(3)
+
+    # Copy and rename the main.exe file to the release folder
+    shutil.copy(os.path.join("dist", "main.exe"), os.path.join("release", exe_name))
+
+    print("Complete.")
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ main.py
@@ -0,0 +1,12 @@
+from reus_runner import initialize
+
+
+def main():
+    initialize()
+    # Your code goes here
+    print("Hello, World!")
+    input("Press Enter to exit...")
+
+
+if __name__ == "__main__":
+    main()
